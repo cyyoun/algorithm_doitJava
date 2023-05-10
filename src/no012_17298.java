81c49c2d10e895c1492fc9946a56e2dde0ac37fd
cyyoun <cyyoun41@gmail.com>
2023-05-10 20:49:54 +0900

    Q012_오큰수_17298

---
+++ src/no012_17298.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class no012_17298 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        Stack<Integer> S = new Stack<>();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(st.nextToken());
+
+        StringBuffer bf = new StringBuffer();
+        for (int i = 0; i < N; i++) {
+            int a = A[i];
+            int j = i + 1;
+            if (j == N) {
+                bf.append(-1 + " ");
+                break;
+            }
+            int b = A[j];
+
+            while (j < N) {
+                if (a < b) {
+                    bf.append(b + " ");
+                    break;
+                } else {
+                    j++;
+                    if (j == N) {
+                        bf.append(-1 + " ");
+                        break;
+                    }
+                    b = A[j];
+                }
+            }
+        }
+        System.out.println(bf.toString().trim());
+    }
+}
