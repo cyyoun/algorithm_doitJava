57c72911ec48e6072961fc5a6d1d04c1ae796846
cyyoun <cyyoun41@gmail.com>
2023-05-12 18:03:07 +0900

    Q014_절댓값힘_11286

--- src/no012_17298.java
+++ src/no012_17298.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class no012_17298 {
@@ -10,7 +9,6 @@ public class no012_17298 {
         int N = Integer.parseInt(bufferedReader.readLine());
 
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        Stack<Integer> S = new Stack<>();
         int[] A = new int[N];
 
         for (int i = 0; i < N; i++)
@@ -21,7 +19,7 @@ public class no012_17298 {
             int a = A[i];
             int j = i + 1;
             if (j == N) {
-                bf.append(-1 + " ");
+                bf.append(-1+" ");
                 break;
             }
             int b = A[j];
