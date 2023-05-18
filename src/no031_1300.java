98d0cab724a6113fcb9e88e44f5cb64314a78e4c
cyyoun <cyyoun41@gmail.com>
2023-05-18 16:27:33 +0900

    Q031_k번째수_1300

---
+++ src/no031_1300.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class no031_1300 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int k = Integer.parseInt(stringTokenizer.nextToken());
+        long start = 1, end = k;
+        long ans = 0;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long cnt = 0;
+
+            for (int i = 0; i <= n; i++) {
+                cnt += Math.min(mid / i, n);
+            }
+
+            if (cnt < k) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
