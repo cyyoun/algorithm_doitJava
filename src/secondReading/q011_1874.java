7b962ba693e06747b382b4089e17a2d592ab5df7
cyyoun <cyyoun41@gmail.com>
2023-06-22 21:12:49 +0900

    DNA 비밀번호 | 스택 수열

---
+++ src/secondReading/q011_1874.java
@@ -0,0 +1,38 @@
+package secondReading;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class q011_1874 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> A = new Stack<>();
+        StringBuilder bf = new StringBuilder();
+        int k = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int m = Integer.parseInt(br.readLine());
+
+            while (k < m) {
+                k++;
+                A.push(k);
+                bf.append("+\n");
+            }
+
+            if (A.peek() == m) {
+                A.pop();
+                bf.append("-\n");
+            } else if (A.peek() > m) {
+                bf.append("NO");
+                break;
+            }
+        }
+        System.out.println(bf.toString().contains("NO") ? "NO" : bf.toString().trim());
+
+    }
+
+}
