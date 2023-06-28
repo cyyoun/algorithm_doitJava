4102a2c14c4d226a2acf8c3e99e3bd7766507ce5
cyyoun <cyyoun41@gmail.com>
2023-06-28 15:36:22 +0900

    바이러스

--- src/study/BOJ/Q2606.java
+++ src/study/BOJ/Q2606.java
@@ -1,4 +1,4 @@
-package secondReading;
+package study.BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,42 +7,41 @@ import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class q023_11724 {
+public class Q2606 {
     static boolean[] visited;
     static ArrayList<Integer>[] A;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        A = new ArrayList[n + 1];
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         visited = new boolean[n + 1];
+        A = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             A[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             A[a].add(b);
             A[b].add(a);
         }
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                DFS(i);
-                cnt++;
-            }
-        }
+
+        visited[1] = true;
+        DFS(1);
         System.out.println(cnt);
     }
+
     private static void DFS(int item) {
-        if (visited[item])
-            return;
-        visited[item] = true;
         for (int i : A[item]) {
-            if (!visited[i])
+            if (!visited[i]) {
+                visited[i] = true;
+                cnt++;
                 DFS(i);
+            }
         }
     }
 }
