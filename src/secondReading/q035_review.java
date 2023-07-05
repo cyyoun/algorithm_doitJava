778b34dbf1d96b32f1ad3162731319cde68e8c1c
cyyoun <cyyoun41@gmail.com>
2023-07-05 12:54:49 +0900

    회의실배정 복습

---
+++ src/secondReading/q035_review.java
@@ -0,0 +1,45 @@
+package secondReading;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class q035_review {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] A = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[i][0] = a;
+            A[i][1] = b;
+        }
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int cnt = 0;
+        int end = -1;
+        for (int i = 0; i < n; i++) {
+            if (A[i][0] >= end) { //시작시간이 end보다 앞인 경우
+                cnt++;
+                end = A[i][1];
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
