1b9ebd85cd5ce7b59e2f0d37ddb5ce3e9253680b
cyyoun <cyyoun41@gmail.com>
2023-07-12 14:26:13 +0900

    달리기 경주

--- src/Programmers/달리기경주.java
+++ src/Programmers/달리기경주.java
@@ -1,36 +1,30 @@
 package Programmers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class 달리기경주 {
     public static void main(String[] args) {
 
         String[] players = {"mumu", "soe", "poe", "kai", "mine"};
         String[] callings = {"kai", "kai", "mine", "mine"};
-        solution(players, callings);
+        System.out.println(Arrays.toString(solution(players, callings)));
+
 
     }
     public static String[] solution(String[] players, String[] callings) {
-
-
-        Map<String, Integer> indexMap = new HashMap<>();
-
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
-            indexMap.put(players[i], i);
+            map.put(players[i], i);
         }
+        for (String call : callings) {
+            int idx = map.get(call);
+            String temp = players[idx];
+            players[idx] = players[idx - 1];
+            players[idx - 1] = temp;
 
-        for (String calling : callings) {
-            int idx = indexMap.get(calling);
-            String temp = players[idx - 1];
-            players[idx - 1] = players[idx];
-            players[idx] = temp;
-
-            indexMap.put(players[idx - 1], idx - 1);
-            indexMap.put(players[idx], idx);
+            map.put(players[idx - 1], idx - 1);
+            map.put(players[idx], idx);
         }
-
-
         return players;
     }
 }
