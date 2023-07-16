14ae7a05926a6a2ba3c690f671c97809112b6d25
cyyoun <cyyoun41@gmail.com>
2023-07-16 20:04:48 +0900

    하샤드 수

---
+++ src/Programmers/하샤드수.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class 하샤드수 {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+    private static boolean solution(int x) {
+
+        int sum = 0;
+        for (char i : String.valueOf(x).toCharArray()) {
+            sum += Integer.parseInt(String.valueOf(i));
+        }
+        return x % sum == 0;
+    }
+}
