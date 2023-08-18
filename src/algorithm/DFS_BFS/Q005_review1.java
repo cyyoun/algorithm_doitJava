b2bec1262fee79271107a62033358b4caf085239
cyyoun <cyyoun41@gmail.com>
2023-08-18 16:48:49 +0900

    2178 복습) 미로 탐색하기

--- src/algorithm/DFS_BFS/Q005_review1.java
+++ src/algorithm/DFS_BFS/Q005_review1.java
@@ -5,34 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-/**
- * 백준 실버 1
- * 2178) 미로 탐색하기
- * time : 156ms
- * memory : 15672KB
- * review : 230817 230818(15508KB, 156ms)
- */
-public class Q005 {
-    static int[][] A;
-    static boolean[][] visited;
+public class Q005_review1 {
     static int m, n;
+    static boolean[][] visited;
+    static int[][] A;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        n = Integer.parseInt(line.split(" ")[0]);
-        m = Integer.parseInt(line.split(" ")[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         A = new int[n][m];
         visited = new boolean[n][m];
-
         for (int i = 0; i < n; i++) {
-            line = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 A[i][j] = Integer.parseInt(line.substring(j, j + 1));
-
             }
         }
-
         BFS(0, 0);
         System.out.println(A[n - 1][m - 1]);
 
@@ -40,25 +31,23 @@ public class Q005 {
 
     private static void BFS(int a, int b) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{a, b});
         int[] dx = {0, 1, 0, -1};
         int[] dy = {1, 0, -1, 0};
+        q.offer(new int[]{a, b});
 
         while (!q.isEmpty()) {
-            int[] arr = q.poll();
+            int[] arrPoll = q.poll();
             for (int i = 0; i < 4; i++) {
-                int x = arr[0] + dx[i];
-                int y = arr[1] + dy[i];
+                int x = dx[i] + arrPoll[0];
+                int y = dy[i] + arrPoll[1];
                 if (x >= 0 && y >= 0 && x < n && y < m) {
                     if (!visited[x][y] && A[x][y] == 1) {
                         visited[x][y] = true;
+                        A[x][y] = A[arrPoll[0]][arrPoll[1]] + 1;
                         q.offer(new int[]{x, y});
-                        A[x][y] = A[arr[0]][arr[1]] + 1;
                     }
                 }
             }
         }
-
-
     }
 }
