acd5e8d8a877bda0b2a9b945c9ec28f73bc54c41
cyyoun <cyyoun41@gmail.com>
2023-08-24 17:08:13 +0900

    17413) 단어 뒤집기 2

---
+++ src/algorithm/implementation/Q010.java
@@ -0,0 +1,52 @@
+package algorithm.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * 백준 실버 3
+ * 17413) 단어 뒤집기 2
+ * time :
+ * memory :
+ * review : 230824(fail)
+ */
+public class Q010 {
+    static Stack<Character> stack;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        stack = new Stack<>();
+        boolean chk = false;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '<') {
+                print();
+                chk = true;
+                System.out.print(line.charAt(i));
+            } else if (line.charAt(i) == '>') {
+                chk = false;
+                System.out.print(line.charAt(i));
+            } else if (chk) {
+                System.out.print(line.charAt(i));
+            }
+            else {
+                if (line.charAt(i) == ' ') {
+                    print();
+                    System.out.print(line.charAt(i));
+                } else {
+                    stack.push(line.charAt(i));
+                }
+            }
+
+        }
+        print();
+    }
+
+    private static void print() {
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
