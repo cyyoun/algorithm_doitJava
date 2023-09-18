65628789a0821bb1afea7073040a53ff7c8b5809
cyyoun <cyyoun41@gmail.com>
2023-09-18 19:25:21 +0900

    review 9655) 돌 게임

---
+++ src/algorithm/Review/Q022_review1.java
@@ -0,0 +1,18 @@
+package algorithm.Review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q022_review1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0) {
+            System.out.println("CY");
+        } else
+            System.out.println("SK");
+    }
+}
