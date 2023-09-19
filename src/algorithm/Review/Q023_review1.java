0e32cadb0c467f5affbf721a26f99da5ff629648
cyyoun <cyyoun41@gmail.com>
2023-09-19 23:56:17 +0900

    review 2193) 이친수

---
+++ src/algorithm/Review/Q023_review1.java
@@ -0,0 +1,20 @@
+package algorithm.Review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q023_review1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] A = new long[n + 1];
+        A[0] = 0;
+        A[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            A[i] = A[i - 1] + A[i - 2];
+        }
+        System.out.println(A[n]);
+
+    }
+}
